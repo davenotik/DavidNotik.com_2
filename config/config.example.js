9bde0a6ee27af74ebdde0b2c8da41042345cc2e5
Notik <dnotik@c4b301c156c7.ant.amazon.com>
2018-12-17 18:44:58 -0800

    Set up Firebase

---
+++ config/config.example.js
@@ -0,0 +1,10 @@
+module.exports = {
+  firebase: {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
+  }
+}
--- src/client/components/app.js
+++ src/client/components/app.js
@@ -3,6 +3,27 @@ import ReactDOM from "react-dom";
 import Header from "./header";
 import css from '../../../static/app.css';
 import Entry from "./entry";
+import firebase from 'firebase';
+import 'firebase/firestore';
+import {firebase as fb} from "../../../config/config";
+
+//Initialize Firebase
+const firebaseApp = firebase.initializeApp(fb);
+const db = firebaseApp.firestore();
+db.settings({timestampsInSnapshots: true});
+
+// var posts = db.collection('posts').get().then(function(posts) {
+//   posts.forEach(function(post) {
+//     console.log(post.data());
+//   });
+// });
+
+db.collection("posts").onSnapshot(function(querySnapshot) {
+  querySnapshot.forEach(function(doc) {
+    // doc.data() is never undefined for query doc snapshots
+    console.log(doc.id, " => ", doc.data());
+  });
+});
 
 
 class App extends Component {
@@ -18,7 +39,6 @@ class App extends Component {
     return (
       <div className="main-container">
         <Header/>
-        <Entry/>
         <Entry/>
         <Entry/>
         <Entry/>
