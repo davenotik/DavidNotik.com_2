41ce9b4b42b2df2e58a48c4a250104d5ef284be4
David Notik <dave@notik.com>
2018-12-24 23:35:02 -0800

    Skeleton block

--- src/client/components/BlogList.js
+++ src/client/components/BlogList.js
@@ -2,9 +2,22 @@ import React, { Component } from "react";
 import BlogPost from "./BlogPost";
 import Skeleton from 'react-loading-skeleton';
 
+function SkeletonBlock() {
+  return(
+    <div>
+      <br/><br/>
+      <Skeleton count={10}/>
+      <br/><br/>
+      <Skeleton count={5}/>
+      <br/><br/>
+      <Skeleton count={10}/>
+    </div>
+  );
+}
+
 function BlogList(props) {
   const content =
-    (props.posts.length === 0) ? <Skeleton count={10}/> : props.posts.map((post) =>
+    (props.posts.length === 0) ? <SkeletonBlock/> : props.posts.map((post) =>
     <BlogPost
       key={post.id}
       title={post.title}
